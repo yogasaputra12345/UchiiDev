c39ee914fdc829c4b067d4668c35c8e013bc570b
yogasaputra12345 <randasaputra.ks@gmail.com>
2022-07-22 20:55:18 +0700

    Ver 1

---
+++ js/style.js
@@ -0,0 +1,42 @@
+
+var navbar = document.querySelector("nav");
+var hamburger = document.querySelector(".header-toggle");
+var close = document.querySelector(".showClose");
+
+var blackBackground = document.querySelector(".blackBackground");
+var blackBackgroundOff = document.querySelector(".blackBackgroundOn");
+
+hamburger.addEventListener("click", toggleHamburgerOn);
+close.addEventListener("click", toggleHamburgerOff);
+blackBackground.addEventListener("click", toggleHamburgerOff);
+
+function toggleHamburgerOn(){
+    navbar.classList.add("showNav");
+    blackBackground.classList.add("blackbackgroundOn");
+}
+
+function toggleHamburgerOff(){
+    navbar.classList.remove("showNav");
+    blackBackground.classList.remove("blackbackgroundOn");
+}
+
+var header = document.querySelector("header");
+var main = document.querySelector("main");
+
+window.onscroll = function(){
+    calcSticky();
+};
+
+var sticky = header.offsetTop;
+
+function calcSticky(){
+    if(window.pageYOffset > sticky){
+        header.classList.remove("header-hidden")
+        header.classList.add("header-sticky")
+        main.classList.add("stickyMargin")
+    }else{
+        header.classList.remove("header-sticky")
+        header.classList.add("header-hidden")
+        main.classList.remove("stickyMargin")
+    }
+}
