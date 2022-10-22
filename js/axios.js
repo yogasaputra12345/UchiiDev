d6a0ecbed8f66a69665df3dbff4bde9f609ba53a
yogasaputra12345 <randasaputra.ks@gmail.com>
2022-10-22 17:27:18 +0700

    trial react 16.2

--- js/axios.js
+++ js/axios.js
@@ -1,15 +1,30 @@
-import axios from 'axios';
 
-const token = '17|2bjIThBL1nVrE8fDjPTygHbp8GtTyuTO8NMdmsmx';
-
-axios.defaults.headers =  {
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: 'Bearer ' + token
+const token = "17|2bjIThBL1nVrE8fDjPTygHbp8GtTyuTO8NMdmsmx"
+    const createTr = user => {
+        const tr = document.createElement('li')
+        // add user details to `li`
+        tr.textContent = `${user.name} (Lv ${user.level}) EXP: ${(user.xp) * 11}, Map: ${user.map.name}`
+        return tr
     }
-}
+    
 
-axios.get('https://toram-id.info/api/v1/items')
-.then((response) => console.log(response.data))
-.catch((err) => console.error(err));
+    const appendToDOM = users => {
+        const table = document.getElementById('list')
+        //iterate over all users
+        users.map(user => {
+            table.appendChild(createTr(user))
+        })
+    }
+    const auth = {
+        headers : {
+            Authorization :`Bearer ` + token
+        }
+    }
+    const apii = new URL(`https://toram-id.info/api/v1/monsters/3`)
+    const playerlv = "200";
+    axios.get(`${apii}?level=${playerlv}&bonusexp=0&between=5`, auth)
+        .then(response => {
+            const users = response.data.data;
+            const sorted = users.sort((elem1, elem2) => elem2.xp - elem1.xp )
+            appendToDOM(sorted)
+    })
