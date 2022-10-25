614a9b005222b8b403fe8e2938e2ce91ec0fe484
yogasaputra12345 <randasaputra.ks@gmail.com>
2022-10-25 12:16:17 +0700

    New lv finder

--- js/axios.js
+++ js/axios.js
@@ -1,30 +1,45 @@
-
-const token = "17|2bjIThBL1nVrE8fDjPTygHbp8GtTyuTO8NMdmsmx"
-    const createTr = user => {
-        const tr = document.createElement('li')
-        // add user details to `li`
-        tr.textContent = `${user.name} (Lv ${user.level}) EXP: ${(user.xp) * 11}, Map: ${user.map.name}`
-        return tr
-    }
-    
-
-    const appendToDOM = users => {
-        const table = document.getElementById('list')
-        //iterate over all users
-        users.map(user => {
-            table.appendChild(createTr(user))
-        })
-    }
-    const auth = {
-        headers : {
-            Authorization :`Bearer ` + token
-        }
-    }
-    const apii = new URL(`https://toram-id.info/api/v1/monsters/3`)
-    const playerlv = "200";
-    axios.get(`${apii}?level=${playerlv}&bonusexp=0&between=5`, auth)
+var PlayerLvl = document.getElementById('level')
+var Submit = document.getElementById('sb')
+function searchfunc(){
+    const token = "17|2bjIThBL1nVrE8fDjPTygHbp8GtTyuTO8NMdmsmx"
+    const auth = {headers : {Authorization :`Bearer ` + token}}
+    axios.get(`https://toram-id.info/api/v1/monsters/3?level=${PlayerLvl.value}&bonusexp=0&between=9`, auth)
         .then(response => {
             const users = response.data.data;
             const sorted = users.sort((elem1, elem2) => elem2.xp - elem1.xp )
-            appendToDOM(sorted)
-    })
+            document.getElementById("app").innerHTML = `
+            <h1 class="app-title">Boss Type</h1>
+            ${sorted.map(bosstemplate).join("")}`;
+            return false
+        })
+}
+Submit.addEventListener('click', searchfunc);
+
+function bonusexp(xp, PlayerLv, BossLv) {
+    if (BossLv <= PlayerLv + 5 && PlayerLv - 5 <= BossLv) {
+        return `${xp * 11}` ;
+    } else if (BossLv <= PlayerLv + 6 && PlayerLv - 6 <= BossLv) {
+        return `${xp * 10}` ;
+    } else if (BossLv <= PlayerLv + 7 && PlayerLv - 7 <= BossLv) {
+        return `${xp * 9}` ;
+    } else if (BossLv <= PlayerLv + 8 && PlayerLv - 8 <= BossLv) {
+        return `${xp * 7}` ;
+    } else if (BossLv <= PlayerLv + 9 && PlayerLv - 9 <= BossLv) {
+        return `${xp * 3}` ;
+    } else {
+        return `${xp}`;
+    }
+}
+function bosstemplate(data0) {
+    return `
+      <div class="animal">
+      <img class="pet-photo" src="${data0.picture}">
+      <h2 class="pet-name"><a href="https://toram-id.info/monster/${data0.id}">${data0.name} 
+      <span class="species">(${bonusexp(data0.xp, PlayerLvl.value, data0.level)} EXP)</span></a></h2>
+      <p><strong>Level:</strong> ${data0.level}</p>
+      <p><strong>Element:</strong> ${data0.element.name}</p>
+      <p><strong>HP:</strong> ${data0.hp}</p>
+      <p><strong>Location:</strong> ${data0.map.name}</p>
+      </div>
+    `;
+}
